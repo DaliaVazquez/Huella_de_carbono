482a951201b78883b6a32015dfaa834cf2b776d0
Dalia <a01635883@itesm.mx>
2021-03-13 13:06:32 -0600

    firestore auth

---
+++ js/config/ConfigFirebase.js
@@ -0,0 +1,9 @@
+const varConfig={
+    apiKey: "AIzaSyD9b8Rv3IRWP8pCapjPlWKz5FAxCOcqVQs",
+    authDomain: "blogeekplatzi-b6151.firebaseapp.com",
+    projectId: "blogeekplatzi-b6151",
+    storageBucket: "blogeekplatzi-b6151.appspot.com",
+    messagingSenderId: "622641669082",
+    appId: "1:622641669082:web:e2221c031397b9b2ad96e3",
+    measurementId: "G-PTMZNGFS55"
+}
